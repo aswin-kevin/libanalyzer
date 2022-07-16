84318614f12a41a845a6273ce7895b1e296c6e4e
aswin-kevin <aswinvenkat60@gmail.com>
2022-07-16 11:33:56 +0530

    Go .mod and .sum support added

--- src/components/Analyzer.ts
+++ src/components/Analyzer.ts
@@ -32,7 +32,7 @@ const getPomDeps = (data: string) => {
   const deps = JSON.parse(data)["dependencies"];
 };
 
-const getGoDeps = async (data: string) => {
+const getGoDepsBySum = async (data: string) => {
   let deps: any = {};
   for (let line of data.split("\n")) {
     let val = line.split(" ");
@@ -62,8 +62,39 @@ const getGoDeps = async (data: string) => {
   // github.com/kubeedge/kubeedge/cloud/pkg/router/listener@1.10.1
 };
 
-const getPyDeps = (data: string) => {
-  console.log(data);
+const getGoDepsByMod = async (data: string) => {
+  let deps: any = {};
+  for (let line of data.split("\n")) {
+    if (line.includes("github.com")) {
+      let val = line
+        .split(" ")
+        .map((le) => le.trim())
+        .filter((el) => el.includes("github.com"));
+      let version = line.trim().match(/(?:(\d+)\.)?(?:(\d+)\.)?(?:(\d+)\.\d+)/);
+      if (version !== null) {
+        let key = val[0].trim();
+        deps[key] = version[0];
+      }
+    }
+  }
+  let depLen = Object.keys(deps).length;
+  let count = 1;
+  showInfoDialog(`Scanning ${depLen} Golang libraries`);
+
+  for (const [key, version] of Object.entries(deps)) {
+    const url = `https://snyk.io/vuln/golang:${key.replaceAll(
+      "/",
+      "%2F"
+    )}@${version}`;
+
+    const vulns = await snykParser(url);
+    deps[key] = { url, vulns, version };
+    if (count === depLen) {
+      return deps;
+    }
+    count += 1;
+  }
+  // github.com/kubeedge/kubeedge/cloud/pkg/router/listener@1.10.1
 };
 
 const analyzeDeps = (ext: any, fileData: string) => {
@@ -78,10 +109,11 @@ const analyzeDeps = (ext: any, fileData: string) => {
   } else if (ext.endsWith(".sum")) {
     console.log("Go project found");
     showWarnDialog("Go project found");
-    return getGoDeps(fileData);
-  } else if (ext.endsWith(".py")) {
-    console.log("Python project found");
-    getPyDeps(fileData);
+    return getGoDepsBySum(fileData);
+  } else if (ext.endsWith(".mod")) {
+    console.log("Go project found");
+    showWarnDialog("Go project found");
+    return getGoDepsByMod(fileData);
   }
 };
 
